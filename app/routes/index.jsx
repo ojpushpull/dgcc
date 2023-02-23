d7b94e480c21f5bba2ebbda1626cbd15d6514668
Okera Johnson <okerajohnson@gmail.com>
2023-02-23 10:14:46 -0500

    brick after brick

---
+++ app/routes/index.jsx
@@ -0,0 +1,14 @@
+export const meta = () => {
+    return {
+        title: 'Hydrogen',
+        descriptioon: 'A custom storefront powered by Hydrogen',
+    };
+};
+
+export default function Index(){
+    return (
+        <div>
+            <h3>Hello from the dog base</h3>
+        </div>
+    );
+}

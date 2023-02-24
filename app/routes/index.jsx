feed9223c3d513a0889eaf39b025e0625cc1f68a
Okera Johnson <okerajohnson@gmail.com>
2023-02-24 16:30:09 -0500

    added collections to dogcare

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from '@remix-run/react';
+import { useLoaderData, Link } from '@remix-run/react';
 export const meta = () => {
     return {
         title: 'Hydrogen',
@@ -12,8 +12,23 @@ export async function loader({context}) {
 
 export default function Index(){
     const {collections} = useLoaderData();
-    console.log(collections);
-    return <p>Hello from the bome pabe</p>;
+    
+    return (
+    <section className="w-full gap-4">
+        <h2 className="whitespace-pre-wrap max-w-prose font-bold text-lead">
+            Collections
+        </h2>
+        <div className="grid-flow-row grid gap-2 gap-y-6 md:gap-4 lg:gap-6 grid-cols-1 false  sm:grid-cols-3 false false">
+    {collections.nodes.map((collection) => {
+        return (
+            <Link to={'/collections/${collection.handle}'} key={collection.id}>
+                {collection.title}
+            </Link>
+        );
+    })}
+    </div>
+    </section>
+    );
 }
 
 const COLLECTIONS_QUERY = `#graphql

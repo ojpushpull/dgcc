697e598bed9b410e7352f4dbc82e6fd22d77d804
Okera Johnson <okerajohnson@gmail.com>
2023-02-26 13:05:33 -0500

    need to ffix colection error

--- app/routes/collections/$handle.jsx
+++ app/routes/collections/$handle.jsx
@@ -1,3 +1,52 @@
+import {useLoaderData} from '@remix-run/react';
+import {json} from '@shopify/remix-oxygen';
+
+export async function loader({params, context}) {
+  const {handle} = params;
+  const {collection} = await context.storefront.query(COLLECTION_QUERY, {
+    variables: {
+      handle,
+    },
+  });
+
+  
+
+  // json is a Remix utility for creating application/json responses
+  // https://remix.run/docs/en/v1/utils/json
+  return json({
+    collection,
+  });
+}
+
 export default function Collection() {
-    return <p>Hello from the collection page!</p>;
+  const {collection} = useLoaderData();
+  return (
+    <>
+      <header className="grid w-full gap-8 py-8 justify-items-start">
+        <h1 className="text-4xl whitespace-pre-wrap font-bold inline-block">
+          {collection.title}
+        </h1>
+
+        {collection.description && (
+          <div className="flex items-baseline justify-between w-full">
+            <div>
+              <p className="max-w-md whitespace-pre-wrap inherit text-copy inline-block">
+                {collection.description}
+              </p>
+            </div>
+          </div>
+        )}
+      </header>
+    </>
+  );
+}
+
+const COLLECTION_QUERY = `#graphql
+  query CollectionDetails($handle: String!) {
+    collection(handle: $handle) {
+      title
+      description
+      handle
+    }
   }
+`;

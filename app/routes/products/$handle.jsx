2ee2fad927784c2f6ab8e3eb8a7f475712314363
Okera Johnson <okerajohnson@gmail.com>
2023-03-02 17:12:46 -0500

    added product qurery data

--- app/routes/products/$handle.jsx
+++ app/routes/products/$handle.jsx
@@ -1,20 +1,41 @@
 import {json} from 'react-router';
 import { useLoaderData } from '@remix-run/react';
 
-export const loader = ({params}) => {
+export const loader = async ({params, context}) => {
     const {handle} = params;
+    const {product} = await context.storefront.query(PRODUCT_QUERY, {
+        variables: {
+            handle,
+        },
+    });
+
+    if (!product?.id) {
+        throw new Response(null, {status: 404});
+    }
 
     return json({
-        handle
+        handle,
+        product,
     });
 }
 
+function PrintJson({data}){
+    return (
+        <details className="outline-outline-2 outline-blue-300 p-4 my-2">
+            <summary>Product Json</summary>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
+
+        </details>
+    )
+}
+
 export default function ProductHandle() {
-    const {handle} = useLoaderData();
+    const {handle, product} = useLoaderData();
 
     return (
         <div className="product-wrapper">
             <h2> Product Handle: {handle} </h2>
+            <PrintJson data={product} />
         </div>
     );
 }

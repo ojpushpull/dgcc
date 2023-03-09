de4253e49114c8f9829b27989720c3f5185af489
Okera Johnson <okerajohnson@gmail.com>
2023-03-09 14:06:06 -0500

    add to bag working!

--- app/routes/cart.jsx
+++ app/routes/cart.jsx
@@ -58,6 +58,62 @@ export async function action({request, context}) {
 
     let status = 200;
     let result;
+    const cartAction = formData.get('cartAction');
+const countryCode = formData.get('countryCode')
+  ? formData.get('countryCode')
+  : null;
+
+switch (cartAction) {
+  case 'ADD_TO_CART':
+    const lines = formData.get('lines')
+      ? JSON.parse(String(formData.get('lines')))
+      : [];
+
+    if (!cartId) {
+      result = await cartCreate({
+        input: countryCode ? {lines, buyerIdentity: {countryCode}} : {lines},
+        storefront,
+      });
+    } else {
+      result = await cartAdd({
+        cartId,
+        lines,
+        storefront,
+      });
+    }
+
+    cartId = result.cart.id;
+    break;
+  case 'REMOVE_FROM_CART':
+    const lineIds = formData.get('linesIds')
+      ? JSON.parse(String(formData.get('linesIds')))
+      : [];
+
+    if (!lineIds.length) {
+      throw new Error('No lines to remove');
+    }
+
+    result = await cartRemove({
+      cartId,
+      lineIds,
+      storefront,
+    });
+
+    cartId = result.cart.id;
+    break;
+  default:
+    throw new Error('Invalid cart action');
+}
+
+/**
+ * The Cart ID may change after each mutation. We need to update it each time in the session.
+ */
+session.set('cartId', cartId);
+headers.set('Set-Cookie', await session.commit());
+
+const {cart, errors} = result;
+return json({cart, errors}, {status, headers});
+
 }
 
 
@@ -118,61 +174,7 @@ export async function cartRemove({cartId, lineIds, storefront}) {
   return cartLinesRemove;
 }
 
-const cartAction = formData.get('cartAction');
-const countryCode = formData.get('countryCode')
-  ? formData.get('countryCode')
-  : null;
 
-switch (cartAction) {
-  case 'ADD_TO_CART':
-    const lines = formData.get('lines')
-      ? JSON.parse(String(formData.get('lines')))
-      : [];
-
-    if (!cartId) {
-      result = await cartCreate({
-        input: countryCode ? {lines, buyerIdentity: {countryCode}} : {lines},
-        storefront,
-      });
-    } else {
-      result = await cartAdd({
-        cartId,
-        lines,
-        storefront,
-      });
-    }
-
-    cartId = result.cart.id;
-    break;
-  case 'REMOVE_FROM_CART':
-    const lineIds = formData.get('linesIds')
-      ? JSON.parse(String(formData.get('linesIds')))
-      : [];
-
-    if (!lineIds.length) {
-      throw new Error('No lines to remove');
-    }
-
-    result = await cartRemove({
-      cartId,
-      lineIds,
-      storefront,
-    });
-
-    cartId = result.cart.id;
-    break;
-  default:
-    throw new Error('Invalid cart action');
-}
-
-/**
- * The Cart ID may change after each mutation. We need to update it each time in the session.
- */
-session.set('cartId', cartId);
-headers.set('Set-Cookie', await session.commit());
-
-const {cart, errors} = result;
-return json({cart, errors}, {status, headers});
 
 
 const USER_ERROR_FRAGMENT = `#graphql

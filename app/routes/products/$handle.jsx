c662e3e1ec46dcde4c4699abf9e963949506c7c4
Okera Johnson <okerajohnson@gmail.com>
2023-03-09 12:10:24 -0500

    insert linkin park lyrics

--- app/routes/products/$handle.jsx
+++ app/routes/products/$handle.jsx
@@ -48,6 +48,29 @@ function PrintJson({data}){
     )
 }
 
+function ProductForm({variantId}) {
+  const [root] = useMatches();
+  const selectedLocale = root?.data?.selectedLocale;
+  const fetcher = useFetcher();
+
+  const lines = [{merchandiseId: variantId, quantity: 1}];
+
+  return (
+    <fetcher.Form action="/cart" method="post">
+      <input type="hidden" name="cartAction" value={'ADD_TO_CART'} />
+      <input
+        type="hidden"
+        name="countryCode"
+        value={selectedLocale?.country ?? 'US'}
+      />
+      <input type="hidden" name="lines" value={JSON.stringify(lines)} />
+      <button className="bg-black text-white px-6 py-3 w-full rounded-md text-center font-medium max-w-[400px]">
+        Add to Bag
+      </button>
+    </fetcher.Form>
+  );
+}
+
 export default function ProductHandle() {
     const {selectedVariant, product} = useLoaderData();
     const orderable = selectedVariant?.availableForSale || false;
@@ -152,28 +175,7 @@ function ProductGallery({media}) {
      );
 }
 
-function ProductForm({variantId}) {
-  const [root] = useMatches();
-  const selectedLocale = root?.data?.selectedLocale;
-  const fetcher = useFetcher();
 
-  const lines = [{merchandiseId: variantId, quantity: 1}];
-
-  return (
-    <fetcher.Form action="/cart" method="post">
-      <input type="hidden" name="cartAction" value={'ADD_TO_CART'} />
-      <input
-        type="hidden"
-        name="countryCode"
-        value={selectedLocale?.country ?? 'US'}
-      />
-      <input type="hidden" name="lines" value={JSON.stringify(lines)} />
-      <button className="bg-black text-white px-6 py-3 w-full rounded-md text-center font-medium max-w-[400px]">
-        Add to Bag
-      </button>
-    </fetcher.Form>
-  );
-}
 
 
 
